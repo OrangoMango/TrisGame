6a1f54c6d804fc11b5236c16652d7b7001a45f99
Orango Mango <orapama06@gmail.com>
2021-06-02 10:05:04 +0200

    Unbeatable AI

--- Tris_Game.py
+++ Tris_Game.py
@@ -38,9 +38,11 @@ class Game:
 				f.grid(column=y, row=x)
 				self.grids.append(f)
 		self.plabel = Label(self.tk, text="Actual player: {0}".format(self.playernames[self.active_player-1]), font="Calibri 20 bold")
-		self.plabel.place(x=0, y=120, anchor="nw")#grid(row=3, column=0, columnspan=3)
+		self.plabel.place(x=0, y=120, anchor="nw")
 		self.timel = Label(self.tk, text="{0:.2f}sec".format(time.time()-self.t1), fg="red", font="Calibri 20 bold")
-		self.timel.place(x=0, y=165, anchor="nw")#.grid(row=3, column=3)
+		self.timel.place(x=0, y=165, anchor="nw")
+		if data[3] == 2 and self.ai: # If AI is first the AI will place
+			self.place_ai()
 	def mainloop(self):
 		while True:
 			if self.gameIsRunning:
@@ -75,11 +77,28 @@ class Game:
 	def set_from_number(self, n, playern): #set symbol from a index
 		y = n // 3
 		x = n % 3
-		self.grids[n].id.config(text=" X " if playern == 1 else " O ")
+		self.grids[n].id.config(text="O" if playern == 1 else "X")
 		self.tk.update()
 		self.tk.update_idletasks()
 		self.actualgame[n] = playern
+	def set_on_random_corner(self):
+		empty_corners = []
+		if self.actualgame[0] == 0:
+			empty_corners.append(0)
+		if self.actualgame[2] == 0:
+			empty_corners.append(2)
+		if self.actualgame[6] == 0:
+			empty_corners.append(6)
+		if self.actualgame[8] == 0:
+			empty_corners.append(8)
+		if empty_corners == []:
+			return None
+		return random.choice(empty_corners)
 	def ai_insert(self): #AI
+		if self.actualgame[4] == 0: # Place symbol on center if possible
+			return 4;
+		elif not (self.set_on_random_corner() is None): # Place symbol on a corner if possible
+			return self.set_on_random_corner()
 		empty = [x for x in range(9) if self.actualgame[x] == 0]
 		emn = len(empty)
 		backup = self.actualgame[:]
@@ -94,9 +113,9 @@ class Game:
 			self.actualgame[empty[x]] = 1
 			if self.victory(1): #if the player can win
 				self.actualgame = backup[:]
-				return empty[x] #return his index
+				return empty[x] #return player index
 		self.actualgame = backup[:]
-		return self.random_set() #if there is no possibilities, return a random index
+		return self.random_set() #if there are no possibilities, return a random index
 	def no_insert(self):
 		return not (0 in self.actualgame) #if the board is full
 	def quit(self):
@@ -104,7 +123,7 @@ class Game:
 	def set_symbol(self, x, y, playern):
 		if self.check_grid(x, y) == 0: #You can only place symbols if the frame doesn't contain another symbol
 			pos = y*3+x
-			self.grids[pos].id.config(text=" X " if playern == 1 else " O ")
+			self.grids[pos].id.config(text="O" if playern == 1 else "X")
 			self.tk.update()
 			self.tk.update_idletasks()
 			self.actualgame[pos] = playern
@@ -120,30 +139,32 @@ class Game:
 				if not self.ai:
 					self.next_player()
 				elif self.ai == True:
-					self.active_player = 2
-					self.plabel.config(text="Actual player: {0}".format(self.playernames[self.active_player-1])) #update label
-					self.tk.update()
-					time.sleep(0.5)
-					self.set_from_number(self.ai_insert(), 2)
-					if self.victory(self.active_player):
-						self.gameIsRunning = False
-						messagebox.showinfo("Game Over", "AI won the game")
-						self.quit()
-					elif self.no_insert():
-						self.gameIsRunning = False
-						messagebox.showinfo("Game Over", "Game has finished, no winner")
-						self.quit()
-					else:
-						self.active_player = 1
-						self.plabel.config(text="Actual player: {0}".format(self.playernames[self.active_player-1])) #update label
-						self.tk.update()
+					self.place_ai()
 				else:
 					raise Exception('No ai is active')
+	def place_ai(self):
+		self.next_player()
+		self.plabel.config(text="Actual player: {0}".format(self.playernames[self.active_player-1])) #update label
+		self.tk.update()
+		time.sleep(0.5)
+		self.set_from_number(self.ai_insert(), 2)
+		if self.victory(self.active_player):
+			self.gameIsRunning = False
+			messagebox.showinfo("Game Over", "AI won the game")
+			self.quit()
+		elif self.no_insert():
+			self.gameIsRunning = False
+			messagebox.showinfo("Game Over", "Game has finished, no winner")
+			self.quit()
+		else:
+			self.next_player()
+			self.plabel.config(text="Actual player: {0}".format(self.playernames[self.active_player-1])) #update label
+			self.tk.update()
 
 class Setup:
 	def __init__(self):
 		self.tk = Tk()
-		self.tk.title("Tris setup")
+		self.tk.title("Tris setup wizard")
 		self.infoboxes = []
 		self.txtvar = StringVar()
 		for x in range(2):
@@ -165,11 +186,20 @@ class Setup:
 		self.aivar = IntVar()
 		self.ck = Checkbutton(self.tk, text="Against AI", variable=self.aivar, command=ck)
 		self.ck.grid(row=2, column=0, columnspan=2)
+		stl = Label(self.tk, text="Who starts?")
+		stl.grid(row=3, column=0)
+		self.whostarts = IntVar()
+		self.whostarts.set(1)
+		r1 = Radiobutton(self.tk, text="Player1", value=1, variable=self.whostarts)
+		r2 = Radiobutton(self.tk, text="Player2", value=2, variable=self.whostarts)
+		r1.grid(row=4, column=0)
+		r2.grid(row=4, column=1)
 		okb = Button(self.tk, text="Save data", command=self.ok)
-		okb.grid(row=3, column=3)
+		okb.grid(row=5, column=3)
 		self.start = False
 	def ok(self):
-		self.results = [self.infoboxes[0][1].get(), self.infoboxes[1][1].get(), bool(self.aivar.get())]
+		#print(self.whostarts.get())
+		self.results = [self.infoboxes[0][1].get(), self.infoboxes[1][1].get(), bool(self.aivar.get()), self.whostarts.get()]
 		if "" in self.results:
 			messagebox.showerror("Invalid input", "Invalid name given")
 			return
